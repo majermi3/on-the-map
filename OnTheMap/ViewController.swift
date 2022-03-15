bbace71402131fd1d467603d57a53af7afcbcbda
Michal Majernik <majernik.miso@gmail.com>
2022-03-15 16:16:13 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Gökce Hatipoglu Majernik on 3/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
