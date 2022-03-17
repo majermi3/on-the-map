65abeeb4e442ac7a1bb41cbda3bdeba53ece9c4d
Michal Majernik <majernik.miso@gmail.com>
2022-03-17 00:04:51 -0500

    Display map and add pins

--- OnTheMap/Controller/MapViewController.swift
+++ OnTheMap/Controller/MapViewController.swift
@@ -2,15 +2,35 @@
 //  MapViewController.swift
 //  OnTheMap
 //
-//  Created by Gökce Hatipoglu Majernik on 3/15/22.
+//  Created by Michal Majernik on 3/15/22.
 //
 
 import Foundation
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UdacityClient.getStudentLocations(limit: 100) { studentInformation, error in
+            // TODO Handle error
+            DispatchQueue.main.async {
+                StudentData.studentInformation = studentInformation
+                for info in studentInformation {
+                    self.addPoint(studentInformation: info)
+                }
+            }
+        }
+    }
+    
+    func addPoint(studentInformation: StudentInformation) {
+        let annotation: MKPointAnnotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(studentInformation.latitude, studentInformation.longitude)
+
+        self.mapView.addAnnotation(annotation)
     }
 }
