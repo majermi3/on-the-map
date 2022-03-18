60e05fbca90d4985a05643f057c711535cd7fc43
Michal Majernik <majernik.miso@gmail.com>
2022-03-17 23:16:36 -0500

    Display pin for new StudentLocation

--- OnTheMap/Controller/CreateStudentInformationStep2ViewController.swift
+++ OnTheMap/Controller/CreateStudentInformationStep2ViewController.swift
@@ -15,6 +15,7 @@ class CreateStudentInformationStep2ViewController: BaseViewController {
     let placeholder = "Enter a Link to Share Here"
     var coordinates: CLLocationCoordinate2D!
     var mapString: String!
+    var studentInformation: StudentInformation!
     
     @IBOutlet weak var linkTextField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
@@ -23,6 +24,32 @@ class CreateStudentInformationStep2ViewController: BaseViewController {
         super.viewDidLoad()
         
         linkTextField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        let date = getFormattedDate()
+        studentInformation = StudentInformation(
+            objectId: "",
+            uniqueKey: UdacityClient.User.id,
+            firstName: UdacityClient.User.firstName,
+            lastName: UdacityClient.User.lastName,
+            mapString: mapString,
+            mediaURL: "",
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            createdAt: date,
+            updatedAt: date
+        )
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let annotation = StudentLocation(studentInformation: studentInformation)
+        mapView.addAnnotation(annotation)
+        
+        let region = MKCoordinateRegion(
+            center: coordinates,
+            latitudinalMeters: CLLocationDistance(exactly: 5000)!,
+            longitudinalMeters: CLLocationDistance(exactly: 5000)!
+        )
+        mapView.setRegion(mapView.regionThatFits(region), animated: true)
     }
     
     @IBAction func submit(_ sender: Any) {
@@ -30,27 +57,13 @@ class CreateStudentInformationStep2ViewController: BaseViewController {
             showErrorMessage(title: "Submit Failed", message: "Please add a Link to Share")
             return
         }
-        var link = linkTextField.text!
+        let link = setMissingScheme(urlString: linkTextField.text!)
         guard isURLValid(urlString: link) else {
             showErrorMessage(title: "Submit Failed", message: "Shared Link is invalid")
             return
         }
         
-        link = setMissingSchema(urlString: link)
-        
-        let date = getFormattedDate()
-        let studentInformation = StudentInformation(
-            objectId: "",
-            uniqueKey: UdacityClient.User.id,
-            firstName: UdacityClient.User.firstName,
-            lastName: UdacityClient.User.lastName,
-            mapString: mapString,
-            mediaURL: link,
-            latitude: coordinates.latitude,
-            longitude: coordinates.longitude,
-            createdAt: date,
-            updatedAt: date
-        )
+        studentInformation.mediaURL = link
         
         UdacityClient.createStudenInformation(studentInformation: studentInformation) { studentInformationResponse, error in
             if let error = error {
@@ -75,9 +88,8 @@ class CreateStudentInformationStep2ViewController: BaseViewController {
         return now.formatted(date: .abbreviated, time: .omitted)
     }
     
-    func setMissingSchema(urlString: String) -> String {
-        let url = URL(string: urlString)
-        if url?.scheme == nil {
+    func setMissingScheme(urlString: String) -> String {
+        if !urlString.hasPrefix("http") {
             return "https://\(urlString)"
         }
         return urlString
@@ -85,6 +97,6 @@ class CreateStudentInformationStep2ViewController: BaseViewController {
     
     func isURLValid(urlString: String) -> Bool {
         let url = URL(string: urlString)
-        return url?.host != nil
+        return url?.host != nil && url?.scheme != nil
     }
 }
